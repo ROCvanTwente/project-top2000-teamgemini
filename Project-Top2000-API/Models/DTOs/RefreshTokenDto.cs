3f46c365fd2a98691924f16bb23504246ccb4b7d
J-S-Wijnbeek <jasperwijnbeek3@gmail.com>
2025-12-02 15:31:01 +0100

    Delete Project-Top2000-API directory

--- Project-Top2000-API/Models/DTOs/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace TemplateJwtProject.Models.DTOs;
-
-public class RefreshTokenDto
-{
-    [Required]
-    public string RefreshToken { get; set; } = string.Empty;
-}

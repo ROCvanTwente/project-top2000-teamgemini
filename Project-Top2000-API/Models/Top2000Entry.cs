4f6edecc749f00daddc984d715ba6bd97ed35c74
Luka1353 <1353284@student.rocvantwente.nl>
2025-12-10 11:06:29 +0100

    small changes
    Some small changes

--- Project-Top2000-API/Models/Top2000Entry.cs
+++ Project-Top2000-API/Models/Top2000Entry.cs
@@ -1,8 +1,13 @@
-﻿namespace TemplateJwtProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TemplateJwtProject.Models
 {
     public class Top2000Entry
     {
         // Composite key: SongId + Year
+        [Key]
+        [ForeignKey("Songs")]
         public int SongId { get; set; }
         public int Year { get; set; }
         public int Position { get; set; }

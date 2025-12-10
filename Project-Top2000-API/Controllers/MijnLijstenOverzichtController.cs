cad04745eeb995f6077846f2ca768b45194a9667
J-S-Wijnbeek <jasperwijnbeek3@gmail.com>
2025-12-10 09:46:54 +0100

    Controllers aangemaakt

---
+++ Project-Top2000-API/Controllers/MijnLijstenOverzichtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateJwtProject.Controllers
+{
+    public class MijnLijstenOverzichtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
